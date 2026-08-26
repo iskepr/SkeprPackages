c9c271b4ed850d473515435221f433a9f661eeb5
مُحَمّد سِيّد <94808133+iskepr@users.noreply.github.com>
2026-08-26 08:17:31 +0300

    اضافة ال init env

--- skepr_core/lib/core/helpers/env_helper.dart
+++ skepr_core/lib/core/helpers/env_helper.dart
@@ -2,6 +2,20 @@ import "package:flutter/foundation.dart";
 import "package:flutter_dotenv/flutter_dotenv.dart";
 
 abstract class EnvHelper {
+  static Future<void> load({
+    String fileName = ".env",
+    List<String> overrideWithFiles = const [],
+    Map<String, String> mergeWith = const {},
+    bool isOptional = false,
+    Parser parser = const Parser(),
+  }) => dotenv.load(
+    fileName: fileName,
+    overrideWithFiles: overrideWithFiles,
+    mergeWith: mergeWith,
+    isOptional: isOptional,
+    parser: parser,
+  );
+
   static String get(String key, {String defaultValue = ""}) {
     if (!kIsWeb) {
       const webEnv = bool.hasEnvironment("FLUTTER_WEB")
